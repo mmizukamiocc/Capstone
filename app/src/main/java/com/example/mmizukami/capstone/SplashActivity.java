446398f4a4a1950691657ca4b7049341ac692e73
snguyen171 <sonngn4@gmail.com>
2016-11-22 03:51:32 -0800

    added splash screen

---
+++ app/src/main/java/com/example/mmizukami/capstone/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.mmizukami.capstone;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask mediaTask = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(mediaTask, 5000);
+    }
+}
