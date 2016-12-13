17334d9c5e8c23a53a6dadcd3379b8d9de6b256a
Ethan Quach <ethan.quach.10@gmail.com>
2016-12-12 19:15:10 -0800

    Javadoc fully added

--- app/src/main/java/com/example/mmizukami/capstone/Relation.java
+++ app/src/main/java/com/example/mmizukami/capstone/Relation.java
@@ -7,40 +7,87 @@ import android.os.Parcelable;
  * Created by Ethan on 12/1/2016.
  */
 
-// TODO: Add testing cases
-
 public class Relation implements Parcelable {
 
     private int relationId;
     private Pet mPet;
     private User mUser;
 
+    /**
+    * 'cnstructor for Relation object.
+    * Takes in an int for the relationId,
+    * a Pet for the mPet field, and a User for the mUser field.
+    * Primarily used to inflate detail layout.
+    *
+    * @author Ethan Quach
+    */
+    
     public Relation(int relation, Pet petToAdd, User userToAdd) {
         relationId = relation;
         mPet = petToAdd;
         mUser = userToAdd;
     }
-    public Relation( Pet petToAdd, User userToAdd) {
+    
+    /**
+    * Alternative constructor for Relation object.
+    * Takes in a Pet for the mPet field and a User for the mUser field.
+    * Used for when a user adds a pet to the database.
+    * 
+    * @Author Ethan Quach
+    */
+    public Relation(Pet petToAdd, User userToAdd) {
         relationId = -1;
         mPet = petToAdd;
         mUser = userToAdd;
     }
+    
+    /**
+    * Getter for mPet.
+    * @return Pet associated with Relation object
+    *
+    * @Aathor Ethan Quach
+    */
     public Pet getPet() {
         return mPet;
     }
 
+    /**
+    * Getter for mUser.
+    * @return User associated with Relation object
+    *
+    * @author Ethan Quach
+    */
     public User getUser() {
         return mUser;
     }
 
+    /**
+    * Setter for mPet.
+    * @param pet The pet to be associated with the Relation object
+    *
+    * @author Ethan Quach
+    */
     public void setPet(Pet pet) {
         mPet = pet;
     }
 
+    /**
+    * Setter for mUser.
+    * @param user The user to be associated with the Relation object
+    *
+    * @author Ethan Quach
+    */
     public void setUser(User user) {
         mUser = user;
     }
 
+    /**
+    * Overloaded constructor for Relation, for use in Intents.
+    * Takes a Parcel object holding a Pet and constructs it.
+    * @param in The Parcel to be built.
+    *
+    * @author Ethan Quach
+    */
     protected Relation(Parcel in) {
         relationId = in.readInt();
         mPet = in.readParcelable(Pet.class.getClassLoader());
